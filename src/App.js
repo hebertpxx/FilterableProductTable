82af77da16c256ef76eea0049f4ed778764bbad8
Roberto Hebert <33330466+hebertpxx@users.noreply.github.com>
2023-02-24 20:43:30 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import "./styles.css";
+
+const PRODUCTS = [
+  { category: "Fruits", name: "Apple", price: "$1", stocked: true },
+  { category: "Fruits", name: "Dragonfruit", price: "$1", stocked: true },
+  { category: "Fruits", name: "Passionfruit", price: "$2", stocked: false },
+  { category: "Vegetable", name: "Spinach", price: "$2", stocked: true },
+  { category: "Vegetable", name: "Pumpkin", price: "$4", stocked: false },
+  { category: "Vegetable", name: "Peas", price: "$1", stocked: true }
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Filterable Product Table</h1>
+      <FilterableProductTable products={PRODUCTS} />
+    </div>
+  );
+}
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+}
+
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colspan="2" className="ProductCategoryRow">
+        {category}
+      </th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+
+    rows.push(<ProductRow product={product} key={product.name} />);
+
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange
+}) {
+  return (
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+      <label>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)}
+        />
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
